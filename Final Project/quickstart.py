3e7c560823b6377316787bd8e705e6f1c36eded8
Salmaan (Sal) Qadir <37133322+JerseyXS@users.noreply.github.com>
2021-05-05 18:57:40 -0400

    Update quickstart.py

--- Final Project/quickstart.py
+++ Final Project/quickstart.py
@@ -145,11 +145,7 @@ def main():
                 # Go through each brightness level in the pattern
                 for level in brightness_levels:
                     for x in range(16):
-                        for y in range(16):
-                            red = int(0)                    # makes 0-1 range > 0-255 range
-                            green = int(255)
-                            blue = int(0)
-                            unicornhathd.set_pixel(x, y, red, green, blue)  # sets pixels on the hat
+                        unicornhathd.set_pixel(x, y, 0, 255, 0)  # sets pixels on the hat
                     unicornhathd.show()                             # show the pixels
                     time.sleep(0.005)                               # tiny gap, sets frames to a smooth 200/sec
                 time.sleep(0.8)                                     # waiting time between heartbeats
