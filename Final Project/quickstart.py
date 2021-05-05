ddb2053bd772172ec574365d1549c36c6c248039
Salmaan (Sal) Qadir <37133322+JerseyXS@users.noreply.github.com>
2021-05-05 18:56:03 -0400

    Update quickstart.py

--- Final Project/quickstart.py
+++ Final Project/quickstart.py
@@ -146,13 +146,9 @@ def main():
                 for level in brightness_levels:
                     for x in range(16):
                         for y in range(16):
-                            h = 0.0  # red
-                            s = 1.0  # saturation at the top of the red scale
-                            v = go_green[x, y] * float(level) / 10     # brightness depends on range
-                            r, g, b = colorsys.hsv_to_rgb(h, s, v)  # convert hsv back to RGB
-                            red = int(r * 255.0)                    # makes 0-1 range > 0-255 range
-                            green = int(g * 255.0)
-                            blue = int(b * 255.0)
+                            red = int(0)                    # makes 0-1 range > 0-255 range
+                            green = int(255)
+                            blue = int(0)
                             unicornhathd.set_pixel(x, y, red, green, blue)  # sets pixels on the hat
                     unicornhathd.show()                             # show the pixels
                     time.sleep(0.005)                               # tiny gap, sets frames to a smooth 200/sec
